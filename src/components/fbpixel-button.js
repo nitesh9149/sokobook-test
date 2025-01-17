1465d943e8f52ea44c457096f700da090341d186
Nitesh <nitesh.rajbanshi@gmail.com>
2025-01-17 14:48:24 +0545

    layout changes

--- src/components/fbpixel-button.js
+++ src/components/fbpixel-button.js
@@ -8,23 +8,21 @@ function FbpixelButton() {
     fbq("track", "googlePlayLink");
   }
   return (
-    <button onClick={handleClick} id="googlePlayLink" className="w-fit h-auto">
-      <Link
-        href={app.googlePlayLink}
-        target="_blank"
-        name="Google Play Link"
-        title="Link to Google Play Store"
-      >
-        <Image
-          src="/google-play-badge.webp"
-          alt="Google play badge"
-          className="object-contain"
-          width={150}
-          height={100}
-        />
-      </Link>
-      <span className="sr-only">Google Play Button</span>
-    </button>
+    <Link
+      href={app.googlePlayLink}
+      target="_blank"
+      name="Google Play Link"
+      title="Link to Google Play Store"
+    >
+      <Image
+        onClick={handleClick}
+        src="/google-play-badge.webp"
+        alt="Google play badge"
+        className="object-contain"
+        width={150}
+        height={100}
+      />
+    </Link>
   );
 }
 
